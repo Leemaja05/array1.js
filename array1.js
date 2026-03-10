da5870d1294172259d80aab729c350f70c3d7241
Leemaja05 <limajjaa05@gmail.com>
2026-03-10 09:50:47 +0700

    Add files via upload

---
+++ array1.js
@@ -0,0 +1,28 @@
+const students = [
+ { name: "Andi", age: 20, major: "Informatika" },
+ { name: "Budi", age: 21, major: "Sistem Informasi" },
+ { name: "Citra", age: 19, major: "Teknik Komputer" }
+];
+
+console.log("Nama mahasiswa pertama: " + students[0].name);
+
+console.log("Jurusan mahasiswa kedua: " + students[1].major);
+
+
+console.log("Umur mahasiswa ketiga: " + students[2].age);
+
+
+
+
+const nilai = [
+ ["Andi", 80],
+ ["Budi", 75],
+ ["Citra", 90],
+ ["Dodi", 85]
+];
+
+console.log("Nama siswa pertama: " + nilai[0][0]);
+
+console.log("Nilai siswa ketiga: " + nilai[2][1]);
+
+console.log("Nama siswa keempat: " + nilai[3][0]);
